02bebc5bb43c50e16eb126f9c94c89d86fd77e98
Ket93 <kevincui113@gmail.com>
2021-11-07 17:32:38 -0500

    React framework

---
+++ main/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="heading">
+        <h1>
+          Financial Assistant
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
