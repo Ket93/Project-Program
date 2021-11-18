46515adc2986a73b6eb52398aedb2bb584afb713
andy-liuu <aliu3160@gmail.com>
2021-11-17 23:24:18 -0500

    demo for using mock data

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,39 @@
 const experss = require('express');
 const Joi = require('joi');
 
-
 const app = experss();
 app.use(experss.json());
+
+
+/** mock data for stock prices, pretend that this is in a database and get updated or whatever 
+AAPL	149.99	1.43
+TSLA	1033.42	-2.83
+GS	404.91	0.64
+GOOGL	2973.56	2.00
+PG	146.56	0.16
+*/
+
+
+const stocks = [
+    {ticker: "AAPL", price: "149.99", yield: "1.43"},
+    {ticker: "TSLA", price: "1033.42", yield: "-2.83"},
+    {ticker: "GS", price: "404.91", yield: "0.64"},
+    {ticker: "GOOGL", price: "293.56", yield: "2.00"},
+    {ticker: "PG", price: "146.56", yield: "0.16"}
+]
+
+
+//give stock ticker, get info
+app.get('/api/stocks/:ticker', (req, res) =>{
+    const stock = stocks.find(c => c.ticker === req.params.ticker);
+    if (!stock) res.status(404).send("Not Found");
+    res.send(stock);
+})
+
+
+
+
+//testing endpoints + tutorial following
 
 const courses = [
     {id: 1, name:"course1"},
@@ -33,11 +63,11 @@ app.post('/api/courses', (req, res) =>{
         name: Joi.string().min(3).required()
     }
 
-    const result = schema.validate(req.body);
-    console.log(result);
+    const result = Joi.validate(req.body, schema);
+    //console.log(result);
 
-    if(!req.body.name || req.body.name.length<3){
-        res.status(400).send("Invalid name");
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
         return;
     }
 
